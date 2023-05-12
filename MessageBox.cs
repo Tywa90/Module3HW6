60705f95ed48ee37d9b3fb4fb2c5c4e3fbaee718
Serhii Amelin <amserega90@gmail.com>
2023-05-12 13:40:35 +0300

    Add project

--- MessageBox.cs
+++ MessageBox.cs
@@ -8,9 +8,39 @@ namespace MessageBoxAsync
 {
     public class MessageBox
     {
-        void Open()
-        {
+        public delegate void WinCloseHandler(State state);
+        public event WinCloseHandler WinClose;
 
+        public async void Open()
+        {
+            string openWindow = "Window is open";
+            Console.WriteLine(openWindow);
+
+            for (int i = 3; i > 0; i--)
+            {
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+
+
+            for (int i = 0; i < 7; i++)
+            {
+                string closeWindow = "Window was closed by user";
+                Console.Write(closeWindow);
+
+                int choice = new Random().Next(0, 2);
+                switch (choice)
+                {
+                    case 0:
+                        WinClose(State.Cancel);
+                        break;
+                    case 1:
+                        WinClose(State.OK);
+                        break;
+                    default:
+                        break;
+                }
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,10 +1,36 @@
-﻿namespace MessageBoxAsync
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace MessageBoxAsync
 {
     public class Program
     {
         static void Main(string[] args)
         {
-            
+            MessageBox msBox = new MessageBox();
+
+            msBox.WinClose += Message;
+            msBox.Open();
+
+            void Message(State state)
+            {
+                if (state == State.Cancel)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(" :: Operation is Canceled");
+                    Console.ResetColor();
+
+                }
+                else if(state == State.OK)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine(" :: Operation is OK");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine("SomeError with State.Type");
+                }
+            }
         }
     }
 }
