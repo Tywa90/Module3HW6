ea20a67946eceb483825570e3ba02208aeaf9f09
Serhii Amelin <amserega90@gmail.com>
2023-05-12 10:47:52 +0300

    start project

---
+++ MessageBox.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MessageBoxAsync
+{
+    public class MessageBox
+    {
+        void Open()
+        {
+
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace MessageBoxAsync
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
